0b5f579a28b17626eed1786a18a2ea0a8a478342
rjptechsolutions <rjptechsolutions9@gmail.com>
2021-04-07 18:11:22 +0530

    first commit

---
+++ src/Components/Data.js
@@ -0,0 +1,15 @@
+export const Data = [
+  {
+    question: "How to contact ?",
+    answer: "By sending email you can contact us",
+  },
+  {
+    question: "How to buy product ?",
+    answer: "Visit your nearest store and buy our products",
+  },
+  {
+    question: "what are payment ways ?",
+    answer:
+      "We accept Google Pay, UPI, PhonePay, Paytm, Netbanking, Debit and credit Card, VISA, Master Rupay Cards too!!!",
+  },
+];
